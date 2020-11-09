65b806bbec28ba01be112d8ac8074f4b49934acd
Chris Johnson <ChrisJohnson0804@gmail.com>
2020-11-09 00:37:54 -0500

    started on a form

--- library.js
+++ library.js
@@ -17,4 +17,38 @@ function Book(title, author, pages, read){
 
 const It = new Book('It', 'Stephen King', 1138, true);
 
-console.log(It.info());
+let myLibrary = [];
+let shelf = document.querySelector("#container");
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+addBookToLibrary(It);
+
+const refreshList = () => {
+    myLibrary.forEach(book => {
+        let newBookFrame = document.createElement('div');
+        shelf.appendChild(newBookFrame);
+        let newBook = document.createElement('ul');
+        newBookFrame.appendChild(newBook);
+        let bookTitle = document.createElement('li');
+        newBook.appendChild(bookTitle);
+        bookTitle.textContent = ("Title: " + book.title);
+        let bookAuthor = document.createElement('li');
+        newBook.appendChild(bookAuthor);
+        bookAuthor.textContent = ("Author: " + book.author);
+        let bookPageCount = document.createElement('li');
+        newBook.appendChild(bookPageCount);
+        bookPageCount.textContent = ("Page Count: " + book.pageCount);
+        let bookRead = document.createElement('li');
+        newBook.appendChild(bookRead);
+        if(book.read == true){
+            bookRead.textContent = ("Has been read");
+        } else {
+            bookRead.textContent = ("Has not been read");
+        }
+    })
+}
+
+refreshList();
