3cd6b994354f2d2b80e5781ab098a65489892c5c
Chris Johnson <ChrisJohnson0804@gmail.com>
2020-11-16 13:33:44 -0500

    everything works now. Still ugly as hell though

--- library.js
+++ library.js
@@ -1,10 +1,11 @@
-function Book(title, author, pages, read, index){
+function Book(title, author, pages, read){
 
     this.title = title
     this.author = author
     this.pageCount = pages
     this.read = read
-    this.index = index
+    this.deleteButton = document.createElement('button')
+    this.markButton = document.createElement('button')
     this.info = function() {
         let readtext = "";
         if(read == true){
@@ -16,49 +17,63 @@ function Book(title, author, pages, read, index){
     }
 }
 
-const It = new Book('It', 'Stephen King', 1138, true, false, 0);
+const It = new Book('It', 'Stephen King', 1138, true);
+const Fake = new Book('Fake Book', 'Fake Author', 15000, false);
 let myLibrary = [];
 let shelf = document.querySelector("#container");
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
-    book.index = myLibrary.indexOf(book);
-
 }
+
 addBookToLibrary(It);
+addBookToLibrary(Fake);
 
-let deleteButtons = Array.from(document.querySelectorAll('.delete'));
-console.log(deleteButtons);
+const removeAllChildNodes = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
-const setDeleteButtons = () => {
-    deleteButtons.forEach(button => {
-        if(!button.classList.contains('listening')){
-            button.classList.add('listening');
-            button.addEventListener('click', () => {
-                myLibrary.splice(parseInt(button.value), 1);
-                button.parentElement.remove();
-                refreshList();
-            });
+const setDeleteButton = (book, frame) => {
+    book.deleteButton.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(book), 1);
+        removeAllChildNodes(frame);
+    });
+}
+
+const setMarkButton = (book, readText) => {
+    book.markButton.addEventListener('click', () => {
+        if (readText.textContent == "Has been read") {
+            readText.textContent = "Has not been read"
+        } else if (readText.textContent == "Has not been read") {
+            readText.textContent = "Has been read";
         }
     });
 }
 
 const refreshList = () => {
+    removeAllChildNodes(shelf);
     myLibrary.forEach(book => {
-        book.index = myLibrary.indexOf(book);
+
         let newBookFrame = document.createElement('div');
         shelf.appendChild(newBookFrame);
+
         let newBook = document.createElement('ul');
         newBookFrame.appendChild(newBook);
+
         let bookTitle = document.createElement('li');
         newBook.appendChild(bookTitle);
         bookTitle.textContent = ("Title: " + book.title);
+
         let bookAuthor = document.createElement('li');
         newBook.appendChild(bookAuthor);
         bookAuthor.textContent = ("Author: " + book.author);
+
         let bookPageCount = document.createElement('li');
         newBook.appendChild(bookPageCount);
         bookPageCount.textContent = ("Page Count: " + book.pageCount);
+
         let bookRead = document.createElement('li');
         newBook.appendChild(bookRead);
         if(book.read == true){
@@ -66,17 +81,20 @@ const refreshList = () => {
         } else {
             bookRead.textContent = ("Has not been read");
         }
-        let deleteButton = document.createElement('button');
-        deleteButton.textContent = "Remove Book";
-        deleteButton.setAttribute('class', 'delete');
-        deleteButton.setAttribute('value', book.index);
-        newBookFrame.appendChild(deleteButton);
-        deleteButtons.push(deleteButton);
-    })
+
+        book.deleteButton.textContent = "Remove Book";
+        book.deleteButton.setAttribute('class', 'delete');
+        newBookFrame.appendChild(book.deleteButton);
+        setDeleteButton(book, newBookFrame);
+
+        book.markButton.textContent = "Mark as Read/Unread";
+        book.markButton.setAttribute('class', 'mark');
+        newBookFrame.appendChild(book.markButton);
+        setMarkButton(book, bookRead);
+    });
 }
 
 refreshList();
-
 
 let submitButton = document.querySelector("#addBook");
 
@@ -86,14 +104,17 @@ submitButton.addEventListener('click', () => {
     let author = formData[1].value;
     let pageCount = formData[2].value;
     let read = true;
-    let index = myLibrary.length;
     if(formData[3].checked == false){
         read = false;
     }
-    let newBook = new Book(title, author, pageCount, read, index);
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pageCount').value = '';
+    document.getElementById('yes').checked = false;
+    document.getElementById('no').checked = false;
+    let newBook = new Book(title, author, pageCount, read);
     addBookToLibrary(newBook);
     refreshList();
-    setDeleteButtons();
 });
 
 
